b9d68abef2b9eaf1f342604300fdd3f21558dacd
robinovitch61 <leorobinovitch@gmail.com>
2019-10-09 18:51:49 -0400

    add rstudio container

--- setup_envs.sh
+++ setup_envs.sh
@@ -13,21 +13,27 @@ mkdir /home/jovyan/work/extensions
 # git clone https://github.com/jupytercalpoly/jupyterlab-richtext-mode.git /home/jovyan/work/extensions/jupyter-scribe && \
 # cd /home/jovyan/work/extensions/jupyter-scribe && \
 # npm install && \
-# npm run build && \
-# jupyter labextension link .
+#      npm run build && \
+#      jupyter labextension link .
 # jlpm build
 # jupyter lab build
 
 # # DrawIO https://github.com/QuantStack/jupyterlab-drawio CURRENTLY NOT WORKING
 # jupyter labextension install jupyterlab-drawio
 
+# # Variable inspector https://github.com/lckr/jupyterlab-variableInspector CURRENTLY NOT WORKING
+# jupyter labextension install @lckr/jupyterlab_variableinspector
+
+# # Go to definition https://github.com/krassowski/jupyterlab-go-to-definition
+# jupyter labextension install @krassowski/jupyterlab_go_to_definition
+
+# Widgets https://github.com/jupyter-widgets/ipywidgets/tree/master/packages/jupyterlab-manager
+jupyter labextension install @jupyter-widgets/jupyterlab-manager
+
 # Latex https://github.com/jupyterlab/jupyterlab-latex
 pip install jupyterlab_latex
 jupyter serverextension enable --sys-prefix jupyterlab_latex
 jupyter labextension install @jupyterlab/latex
-
-# Go to definition https://github.com/krassowski/jupyterlab-go-to-definition
-jupyter labextension install @krassowski/jupyterlab_go_to_definition
 
 # Table of contents https://github.com/ian-r-rose/jupyterlab-toc
 jupyter labextension install @jupyterlab/toc
@@ -43,17 +49,19 @@ echo "Finished creating kernel py37!"
 echo ""
 echo "Installing packages in py37..."
 source activate py37 && \
-pip install ipykernel && \
-pip install pandas && \
-pip install numpy && \
-pip install scipy && \
-pip install matplotlib && \
-pip install seaborn && \
+     pip install ipykernel && \
+     pip install pandas && \
+     pip install numpy && \
+     pip install scipy && \
+     pip install matplotlib && \
+     pip install seaborn && \
+     pip install ipywidgets
 echo "Finished installing packages in py37!"
 
 echo ""
 echo "Connecting env to kernel..."
-source activate py37 && python -m ipykernel install --user --name py37 --display-name "py37"
+source activate py37 && \
+     python -m ipykernel install --user --name py37 --display-name "py37"
 source activate py37 # must activate/deactivate once to have changes displayed
 source activate base
 echo "Connected env to kernel!"
